6b5b7921f67b5c414ce5c26dacc735a9432be787
onetuks <onetuks@naver.com>
2024-09-20 15:18:17 +0900

    Initial Commit

---
+++ src/main/java/com/onetuks/threekingdomwikiserver/ThreeKingdomWikiServerApplication.java
@@ -0,0 +1,13 @@
+package com.onetuks.threekingdomwikiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeKingdomWikiServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ThreeKingdomWikiServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/onetuks/threekingdomwikiserver/TestThreeKingdomWikiServerApplication.java
@@ -0,0 +1,12 @@
+package com.onetuks.threekingdomwikiserver;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestThreeKingdomWikiServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.from(ThreeKingdomWikiServerApplication::main)
+        .with(TestcontainersConfiguration.class).run(args);
+  }
+
+}
